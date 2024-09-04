e5a9ec125438dc6be44b5c4e0df0753daebfba9f
ScarlR <scarletraunak@gmail.com>
2024-09-04 23:30:54 +0530

    cors fixed

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ const app = express();
 app.use(express.json());
 
 
-app.use(cors());
+app.use(function (req, res, next) {
+   
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 export const instance =stripe(process.env.STRIPE_SECRET)
 
 
