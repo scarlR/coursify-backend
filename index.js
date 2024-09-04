e291f3cfd6079b34e073c8eb92f2a7aef15514b5
ScarlR <scarletraunak@gmail.com>
2024-09-04 23:58:11 +0530

    cors fixed again

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(
 cors({
 origin: "http://localhost:5173", 
 credentials: true, 
-methods: ["GET", "OST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 allowedHeaders: [
 "Origin",
 "Content-Type",
